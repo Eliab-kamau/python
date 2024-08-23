1371cf33932c8b1108043ef85ae7956171416a58
Eliab-kamau <karekiaeliab@gmail.com>
2024-08-23 17:18:20 +0300

    introduction to python

--- hello.py
+++ hello.py
@@ -4,3 +4,9 @@ print("hello there")
 sum= 4+5
 print(sum)
 print("python programming language")
+
+
+#inserting a break makes line move to the next line
+
+print("python is wonderful to learn\n Its the easiest to learn")
+
